c76e74c3403dfadff43caab40d05ca8ebf33f2f1
andersonsanchez53@gmail.com <guagua76>
2018-09-30 22:39:29 -0700

    Implement zip extraction capability

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'dart:async';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter_document_picker/flutter_document_picker.dart';
+import 'package:archive/archive.dart';
+import 'package:archive/archive_io.dart';
+import 'package:path/path.dart';
 
 class HomePage extends StatefulWidget{
   @override
@@ -8,153 +13,73 @@ class HomePage extends StatefulWidget{
 }
 
 class HomePageState extends State<HomePage>{
-
-  String _path = '-';
+  String _localFilePath = "";
+  String status = "";
   bool _pickFileInProgress = false;
-  bool _iosPublicDataUTI = true;
-  bool _checkByCustomExtension = false;
 
-  final _utiController = TextEditingController(
-    text: 'com.sidlatau.example.mwfbak',
-  );
+  Future<String> get _localPath async{
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
 
-  final _extensionController = TextEditingController(
-    text: 'mwfbak',
-  );
-
-  _documentPicker() async{
+  _localFile() async {
     String result;
     try{
-      setState((){
-        _path = '-';
+      setState(() {
         _pickFileInProgress = true;
       });
-
-      FlutterDocumentPickerParams params = FlutterDocumentPickerParams(
-        allowedFileExtensions:
-        _checkByCustomExtension ? [_extensionController.text] : null,
-        allowedUtiTypes: _iosPublicDataUTI ? null : [_utiController.text],
-      );
-
-      result = await FlutterDocumentPicker.openDocument(params: params);
-    } catch (e){
-      result = "Error: $e";
+      result = await FlutterDocumentPicker.openDocument();
+    } catch (e) {
+      print(e);
+      result = 'Error: $e';
     } finally {
       setState(() {
         _pickFileInProgress = false;
+        _localFilePath = result;
+        status = "File chosen: " + basename(result);
       });
     }
-
-    setState(() {
-      _path = result;
-    });
   }
 
-  _buildIOSParams() {
-    return Padding(
-      padding: EdgeInsets.only(top: 24.0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Text(
-                  'iOS Params',
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              ),
-              Text(
-                'Example app is configured to pick custom document type with extension ".mwfbak"',
-                style: Theme.of(context).textTheme.body1,
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      'Allow pick all documents("public.data" UTI will be used).',
-                      softWrap: true,
-                    ),
-                  ),
-                  Checkbox(
-                    value: _iosPublicDataUTI,
-                    onChanged: (value) {
-                      setState(() {
-                        _iosPublicDataUTI = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              TextField(
-                controller: _utiController,
-                enabled: !_iosPublicDataUTI,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Uniform Type Identifier to pick:',
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+  _unzipFile() async {
+    File fileToUnzip = File(_localFilePath);
+    String result;
+    final path = await _localPath;
+    if (basename(fileToUnzip.path).contains("zip")) {
+      try {
+        var inputStream = await fileToUnzip.readAsBytes();
+        Archive archive = new ZipDecoder().decodeBytes(inputStream);
+
+        for (ArchiveFile file in archive) {
+          String fileName = file.name;
+          if (file.isFile) {
+            List<int> data = file.content;
+            new File(path + "/" + fileName)
+              ..createSync(recursive: true)
+              ..writeAsBytesSync(data);
+          } else {
+            new Directory(path + '/' + fileName)
+              ..create(recursive: true);
+          }
+          result = "Success";
+        }
+      } catch (e) {
+        result = "Error: $e";
+      } finally {
+        setState(() {
+          _localFilePath = result;
+          status = result;
+        });
+      }
+    }
+
+
   }
 
-  _buildCommonParams() {
-    return Padding(
-      padding: EdgeInsets.only(top: 24.0),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Text(
-                  'Common Params',
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Text(
-                        'Check file by extension - if picked file does not have wantent extension - return "extension_mismatch" error',
-                        softWrap: true,
-                      ),
-                    ),
-                  ),
-                  Checkbox(
-                    value: _checkByCustomExtension,
-                    onChanged: (value) {
-                      setState(() {
-                        _checkByCustomExtension = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              TextField(
-                controller: _extensionController,
-                enabled: _checkByCustomExtension,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'File extension to pick:',
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+  @override
+  void initState() {
+    super.initState();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -167,17 +92,16 @@ class HomePageState extends State<HomePage>{
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              _button("Pick a File", _documentPicker),
-              Text(
-                'Picked file path:',
-                style: Theme.of(context).textTheme.title,
+              new MaterialButton(
+                  height: 100.0,
+                  child: Text("Open File"),
+                  onPressed: _localFile),
+              Text("$status"),
+              new MaterialButton(
+                height: 100.0,
+                child: Text("Extract"),
+                onPressed: _unzipFile,
               ),
-              Text('$_path'),
-              _pickFileInProgress ? CircularProgressIndicator() : Container(),
-              _buildCommonParams(),
-              Theme.of(context).platform == TargetPlatform.iOS
-                ? _buildIOSParams()
-                : Container(),
             ],
           ),
         )
@@ -185,13 +109,4 @@ class HomePageState extends State<HomePage>{
     );
   }
 
-  Widget _button(String name, Function f){
-    return MaterialButton(
-      height: 100.0,
-      child: Text (name, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
-      textColor: Colors.black,
-      color: Colors.grey[100],
-      onPressed: f,
-    );
-  }
 }
